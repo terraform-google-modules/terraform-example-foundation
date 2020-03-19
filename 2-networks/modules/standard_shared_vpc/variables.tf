33f4c5884c29b79d4eb98f996e918e5f1942fd9d
Rohan Jerrems <rjerrems@google.com>
2020-03-19 13:31:52 +1100

    feat: Add initial networking code

---
+++ 2-networks/modules/standard_shared_vpc/variables.tf
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "project_id" {
+  type        = string
+  description = "Project ID for Shared VPC."
+}
+
+variable "network_name" {
+  type        = string
+  description = "Name for VPC."
+}
+
+variable "default_region" {
+  type        = string
+  description = "Default subnet region standard_shared_vpc currently only configures one region."
+}
+
+variable "subnets" {
+  type        = list(map(string))
+  description = "The list of subnets being created"
+  default     = []
+}
+
+variable "secondary_ranges" {
+  type        = map(list(object({ range_name = string, ip_cidr_range = string })))
+  description = "Secondary ranges that will be used in some of the subnets"
+  default     = {}
+}
+
+variable "private_service_cidr" {
+  type        = string
+  description = "CIDR range for private service networking. Used for Cloud SQL and other managed services."
+}
+
+variable "bgp_asn" {
+  type        = string
+  description = "BGP ASN for default cloud router."
+}
+

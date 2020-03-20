85557f22943563f8d03b95ba1b87b17b8a053867
Eric Zhao <zhaoe@google.com>
2020-03-20 17:16:24 +1100

    initial

---
+++ 3-projects/variables.tf
@@ -0,0 +1,15 @@
+variable terraform_service_account {
+    type = string
+}
+
+variable organization_id {
+    type = string 
+}
+
+variable billing_account {
+    type = string
+}
+
+variable default_region {
+    type = string
+}

c864c842489c88b0033c7051ed434301839989ad
David Holsgrove <david@apnic.net>
2020-05-27 11:00:45 +1000

    Separate IAM resources and add member for monitoring workspaces editor

--- 1-org/log_sinks.tf
+++ 1-org/log_sinks.tf
@@ -89,22 +89,6 @@ module "bq_data_access_logs" {
 }
 
 /******************************************
-  Audit Logs - IAM
-*****************************************/
-
-resource "google_project_iam_member" "audit_log_bq_user" {
-  project = module.org_audit_logs.project_id
-  role    = "roles/bigquery.user"
-  member  = "group:${var.audit_data_users}"
-}
-
-resource "google_project_iam_member" "audit_log_bq_data_viewer" {
-  project = module.org_audit_logs.project_id
-  role    = "roles/bigquery.dataViewer"
-  member  = "group:${var.audit_data_users}"
-}
-
-/******************************************
   Billing logs (Export configured manually)
 *****************************************/
 
@@ -114,16 +98,3 @@ resource "google_bigquery_dataset" "billing_dataset" {
   friendly_name = "GCP Billing Data"
   location      = var.default_region
 }
-
-resource "google_project_iam_member" "billing_bq_user" {
-  project = module.org_billing_logs.project_id
-  role    = "roles/bigquery.user"
-  member  = "group:${var.billing_data_users}"
-}
-
-resource "google_project_iam_member" "billing_bq_viewer" {
-  project = module.org_billing_logs.project_id
-  role    = "roles/bigquery.dataViewer"
-  member  = "group:${var.billing_data_users}"
-}
-

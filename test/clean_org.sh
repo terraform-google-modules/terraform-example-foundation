2505cb1340d34bedbdde56989bf0b836b21e6984
bharathkkb <bharathkrishnakb@gmail.com>
2020-06-30 01:54:48 -0500

    add framework
    add scc notif name
    
    add securitycenter api
    
    add securitycenter roles
    
    fix destroy
    
    add cleaner script
    
    set project in cleaner script
    
    add networks
    
    fix apis
    
    rand suffix for managed zones
    
    pr comments

---
+++ test/clean_org.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# clean up access-context-manager policies and scc notifications if any
+
+source /usr/local/bin/task_helper_functions.sh && source_test_env && init_credentials
+gcloud config set project "${TF_VAR_project_id:?}"
+sleep 60
+gcloud alpha scc notifications delete test-scc-notification --organization "${TF_VAR_org_id:?}" -q || true
+POLICY_ID=$(gcloud access-context-manager policies list --organization="${TF_VAR_org_id:?}" --format="value(name)")
+gcloud access-context-manager policies delete "${POLICY_ID}" -q || true
---
+++ test/fixtures/org/main.tf
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "test" {
+  source                    = "../../../1-org"
+  parent_folder             = var.parent_folder
+  org_id                    = var.org_id
+  billing_account           = var.billing_account
+  terraform_service_account = var.terraform_sa_email
+  default_region            = "us-central1"
+  billing_data_users        = var.group_email
+  audit_data_users          = var.group_email
+  scc_notification_name     = "test-scc-notification"
+  domains_to_allow          = [var.domain_to_allow]
+}

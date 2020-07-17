11ddf8deb6727a1692bb6b1aa79a624454fdfda9
Rohan Jerrems <rjerrems@google.com>
2020-07-17 14:51:09 +1000

    Remove sudo as metadata scripts run as root

--- 0-bootstrap/modules/jenkins-agent/files/jenkins_gce_startup_script.sh
+++ 0-bootstrap/modules/jenkins-agent/files/jenkins_gce_startup_script.sh
@@ -16,30 +16,30 @@
 #!/bin/sh
 
 echo "**** Startup Step 1/7: Update apt-get repositories. ****"
-sudo apt-get update
+apt-get update
 
 echo "**** Startup Step 2/7: Install Java. Needed to accept jobs from Jenkins Master. ****"
-sudo apt-get install -y default-jdk
+apt-get install -y default-jdk
 
 echo "**** Startup Step 3/7: Install tools needed to run pipeline commands. ****"
-sudo apt-get install -y git jq unzip google-cloud-sdk google-cloud-sdk
+apt-get install -y git jq unzip google-cloud-sdk google-cloud-sdk
 
 echo "**** Startup Step 4/7: Create a directory to locate Terraform binaries. ****"
 # shellcheck disable=SC2154
-sudo mkdir -p "${tpl_TERRAFORM_DIR}" && cd "${tpl_TERRAFORM_DIR}" || exit
+mkdir -p "${tpl_TERRAFORM_DIR}" && cd "${tpl_TERRAFORM_DIR}" || exit
 
 echo "**** Startup Step 5/7: Download, verify and unzip Terraform binaries. ****"
 # shellcheck disable=SC2154
 wget "https://releases.hashicorp.com/terraform/${tpl_TERRAFORM_VERSION}/terraform_${tpl_TERRAFORM_VERSION}_linux_amd64.zip" && \
     echo "${tpl_TERRAFORM_VERSION_SHA256SUM} terraform_${tpl_TERRAFORM_VERSION}_linux_amd64.zip" > terraform_SHA256SUMS && \
     sha256sum -c terraform_SHA256SUMS --status && \
-    sudo unzip "terraform_${tpl_TERRAFORM_VERSION}_linux_amd64.zip" -d "${tpl_TERRAFORM_DIR}" && \
-    sudo chmod 755 terraform && \
-    sudo rm -f "${tpl_TERRAFORM_DIR}terraform_${tpl_TERRAFORM_VERSION}_linux_amd64.zip" && \
-    sudo apt-get remove --purge -y curl unzip && \
-    sudo apt-get --purge -y autoremove && \
-    sudo apt-get clean && \
-    sudo rm -rf /var/lib/apt/lists/*
+    unzip "terraform_${tpl_TERRAFORM_VERSION}_linux_amd64.zip" -d "${tpl_TERRAFORM_DIR}" && \
+    chmod 755 terraform && \
+    rm -f "${tpl_TERRAFORM_DIR}terraform_${tpl_TERRAFORM_VERSION}_linux_amd64.zip" && \
+    apt-get remove --purge -y curl unzip && \
+    apt-get --purge -y autoremove && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
 echo "**** Startup Step 6/7: Download and install the Terraform validator ****"
 gsutil cp gs://terraform-validator/releases/2019-04-04/terraform-validator-linux-amd64 .

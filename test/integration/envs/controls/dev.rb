6bf6a073efa0fa39f69fb17f78951e278eb604bc
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2020-07-21 15:31:45 -0300

    Fixes prod env tests

--- test/integration/envs/controls/dev.rb
+++ test/integration/envs/controls/dev.rb
@@ -13,158 +13,157 @@
 # limitations under the License.
 
 dev_env_folder = attribute('dev_env_folder')
-dev_monitoring_editor_role = attribute('dev_monitoring_editor_role')
 dev_monitoring_project_id = attribute('dev_monitoring_project_id')
 dev_base_shared_vpc_project_id = attribute('dev_base_shared_vpc_project_id')
 dev_restricted_shared_vpc_project_id = attribute('dev_restricted_shared_vpc_project_id')
 dev_env_secrets_project_id = attribute('dev_env_secrets_project_id')
 
 control dev do
-   title 'gcp step 2-envs test dev'
+  title 'gcp step 2-envs test dev'
 
-   describe google_resourcemanager_folder(name: dev_env_folder) do
-      it {should exist}
-      its('display_name') { should eq 'dev' }
-   end
+  describe google_resourcemanager_folder(name: dev_env_folder) do
+    it { should exist }
+    its('display_name') { should eq 'dev' }
+  end
 
-   describe google_project(name: dev_monitoring_project_id) do
-      it {should exist}
-      its('project_id') { should cmp dev_monitoring_project_id }
-      its('lifecycle_state') { should cmp 'ACTIVE' }
-   end
+  describe google_project(name: dev_monitoring_project_id) do
+    it { should exist }
+    its('project_id') { should cmp dev_monitoring_project_id }
+    its('lifecycle_state') { should cmp 'ACTIVE' }
+  end
 
-   describe google_project(name: dev_base_shared_vpc_project_id) do
-      it {should exist}
-      its('project_id') { should cmp dev_base_shared_vpc_project_id }
-      its('lifecycle_state') { should cmp 'ACTIVE' }
-   end
+  describe google_project(name: dev_base_shared_vpc_project_id) do
+    it { should exist }
+    its('project_id') { should cmp dev_base_shared_vpc_project_id }
+    its('lifecycle_state') { should cmp 'ACTIVE' }
+  end
 
-   describe google_project(name: dev_restricted_shared_vpc_project_id) do
-      it {should exist}
-      its('project_id') { should cmp dev_restricted_shared_vpc_project_id }
-      its('lifecycle_state') { should cmp 'ACTIVE' }
-   end
+  describe google_project(name: dev_restricted_shared_vpc_project_id) do
+    it { should exist }
+    its('project_id') { should cmp dev_restricted_shared_vpc_project_id }
+    its('lifecycle_state') { should cmp 'ACTIVE' }
+  end
 
-   describe google_project(name: dev_env_secrets_project_id) do
-      it {should exist}
-      its('project_id') { should cmp dev_env_secrets_project_id }
-      its('lifecycle_state') { should cmp 'ACTIVE' }
-   end
+  describe google_project(name: dev_env_secrets_project_id) do
+    it { should exist }
+    its('project_id') { should cmp dev_env_secrets_project_id }
+    its('lifecycle_state') { should cmp 'ACTIVE' }
+  end
 
-   describe google_project_service(
-      project: dev_monitoring_project_id,
-      name: 'logging.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_monitoring_project_id,
+    name: 'logging.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_monitoring_project_id,
-      name: 'monitoring.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_monitoring_project_id,
+    name: 'monitoring.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_base_shared_vpc_project_id,
-      name: 'compute.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_base_shared_vpc_project_id,
+    name: 'compute.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_base_shared_vpc_project_id,
-      name: 'dns.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_base_shared_vpc_project_id,
+    name: 'dns.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_base_shared_vpc_project_id,
-      name: 'servicenetworking.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_base_shared_vpc_project_id,
+    name: 'servicenetworking.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_base_shared_vpc_project_id,
-      name: 'container.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_base_shared_vpc_project_id,
+    name: 'container.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_base_shared_vpc_project_id,
-      name: 'logging.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_base_shared_vpc_project_id,
+    name: 'logging.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_restricted_shared_vpc_project_id,
-      name: 'compute.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_restricted_shared_vpc_project_id,
+    name: 'compute.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_restricted_shared_vpc_project_id,
-      name: 'dns.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_restricted_shared_vpc_project_id,
+    name: 'dns.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_restricted_shared_vpc_project_id,
-      name: 'servicenetworking.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_restricted_shared_vpc_project_id,
+    name: 'servicenetworking.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_restricted_shared_vpc_project_id,
-      name: 'container.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_restricted_shared_vpc_project_id,
+    name: 'container.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_restricted_shared_vpc_project_id,
-      name: 'logging.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_restricted_shared_vpc_project_id,
+    name: 'logging.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_env_secrets_project_id,
-      name: 'secretmanager.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_env_secrets_project_id,
+    name: 'secretmanager.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: dev_env_secrets_project_id,
-      name: 'logging.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: dev_env_secrets_project_id,
+    name: 'logging.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   google_project_iam_bindings(project: dev_monitoring_project_id).where(iam_binding_role: "roles/monitoring.editor") do
-      it { should exist }
-      # its('members') {should include 'group:${var.monitoring_workspace_users}' }
-   end
+  google_project_iam_bindings(project: dev_monitoring_project_id).where(iam_binding_role: 'roles/monitoring.editor') do
+    it { should exist }
+    # its('members') {should include 'group:${var.monitoring_workspace_users}' }
+  end
 end
--- test/integration/envs/controls/prod.rb
+++ test/integration/envs/controls/prod.rb
@@ -13,158 +13,157 @@
 # limitations under the License.
 
 prod_env_folder = attribute('prod_env_folder')
-prod_monitoring_editor_role = attribute('prod_monitoring_editor_role')
 prod_monitoring_project_id = attribute('prod_monitoring_project_id')
 prod_base_shared_vpc_project_id = attribute('prod_base_shared_vpc_project_id')
 prod_restricted_shared_vpc_project_id = attribute('prod_restricted_shared_vpc_project_id')
 prod_env_secrets_project_id = attribute('prod_env_secrets_project_id')
 
-control nonprod do
-   title 'gcp step 2-envs test nonprod'
+control prod do
+  title 'gcp step 2-envs test prod'
 
-   describe google_resourcemanager_folder(name: nonprod_env_folder) do
-      it {should exist}
-      its('display_name') { should eq 'nonprod' }
-   end
+  describe google_resourcemanager_folder(name: prod_env_folder) do
+    it { should exist }
+    its('display_name') { should eq 'prod' }
+  end
 
-   describe google_project(name: nonprod_monitoring_project_id) do
-      it {should exist}
-      its('project_id') { should cmp nonprod_monitoring_project_id }
-      its('lifecycle_state') { should cmp 'ACTIVE' }
-   end
+  describe google_project(name: prod_monitoring_project_id) do
+    it { should exist }
+    its('project_id') { should cmp prod_monitoring_project_id }
+    its('lifecycle_state') { should cmp 'ACTIVE' }
+  end
 
-   describe google_project(name: nonprod_base_shared_vpc_project_id) do
-      it {should exist}
-      its('project_id') { should cmp nonprod_base_shared_vpc_project_id }
-      its('lifecycle_state') { should cmp 'ACTIVE' }
-   end
+  describe google_project(name: prod_base_shared_vpc_project_id) do
+    it { should exist }
+    its('project_id') { should cmp prod_base_shared_vpc_project_id }
+    its('lifecycle_state') { should cmp 'ACTIVE' }
+  end
 
-   describe google_project(name: nonprod_restricted_shared_vpc_project_id) do
-      it {should exist}
-      its('project_id') { should cmp nonprod_restricted_shared_vpc_project_id }
-      its('lifecycle_state') { should cmp 'ACTIVE' }
-   end
+  describe google_project(name: prod_restricted_shared_vpc_project_id) do
+    it { should exist }
+    its('project_id') { should cmp prod_restricted_shared_vpc_project_id }
+    its('lifecycle_state') { should cmp 'ACTIVE' }
+  end
 
-   describe google_project(name: nonprod_env_secrets_project_id) do
-      it {should exist}
-      its('project_id') { should cmp nonprod_env_secrets_project_id }
-      its('lifecycle_state') { should cmp 'ACTIVE' }
-   end
+  describe google_project(name: prod_env_secrets_project_id) do
+    it { should exist }
+    its('project_id') { should cmp prod_env_secrets_project_id }
+    its('lifecycle_state') { should cmp 'ACTIVE' }
+  end
 
-   describe google_project_service(
-      project: nonprod_monitoring_project_id,
-      name: 'logging.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_monitoring_project_id,
+    name: 'logging.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_monitoring_project_id,
-      name: 'monitoring.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_monitoring_project_id,
+    name: 'monitoring.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_base_shared_vpc_project_id,
-      name: 'compute.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_base_shared_vpc_project_id,
+    name: 'compute.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_base_shared_vpc_project_id,
-      name: 'dns.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_base_shared_vpc_project_id,
+    name: 'dns.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_base_shared_vpc_project_id,
-      name: 'servicenetworking.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_base_shared_vpc_project_id,
+    name: 'servicenetworking.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_base_shared_vpc_project_id,
-      name: 'container.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_base_shared_vpc_project_id,
+    name: 'container.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_base_shared_vpc_project_id,
-      name: 'logging.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_base_shared_vpc_project_id,
+    name: 'logging.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_restricted_shared_vpc_project_id,
-      name: 'compute.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_restricted_shared_vpc_project_id,
+    name: 'compute.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_restricted_shared_vpc_project_id,
-      name: 'dns.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_restricted_shared_vpc_project_id,
+    name: 'dns.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_restricted_shared_vpc_project_id,
-      name: 'servicenetworking.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_restricted_shared_vpc_project_id,
+    name: 'servicenetworking.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_restricted_shared_vpc_project_id,
-      name: 'container.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_restricted_shared_vpc_project_id,
+    name: 'container.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_restricted_shared_vpc_project_id,
-      name: 'logging.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_restricted_shared_vpc_project_id,
+    name: 'logging.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_env_secrets_project_id,
-      name: 'secretmanager.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_env_secrets_project_id,
+    name: 'secretmanager.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   describe google_project_service(
-      project: nonprod_env_secrets_project_id,
-      name: 'logging.googleapis.com'
-   ) do
-      it { should exist }
-      its('state') { should cmp 'ENABLED' }
-   end
+  describe google_project_service(
+    project: prod_env_secrets_project_id,
+    name: 'logging.googleapis.com'
+  ) do
+    it { should exist }
+    its('state') { should cmp 'ENABLED' }
+  end
 
-   google_project_iam_bindings(project: nonprod_monitoring_project_id).where(iam_binding_role: "roles/monitoring.editor") do
-      it { should exist }
-      # its('members') {should include 'group:${var.monitoring_workspace_users}' }
-   end
+  google_project_iam_bindings(project: prod_monitoring_project_id).where(iam_binding_role: 'roles/monitoring.editor') do
+    it { should exist }
+    # its('members') {should include 'group:${var.monitoring_workspace_users}' }
+  end
 end

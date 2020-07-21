a8b02e4f0e978f88e0c5cd3aeb46ff2a15c3c54c
Mike Laramie <mikelaramie@google.com>
2020-07-20 21:57:02 -0400

    fixed linting in tf-wrapper.sh

--- build/tf-wrapper.sh
+++ build/tf-wrapper.sh
@@ -1,9 +1,19 @@
-##TODO:  better error handling
-##TODO:  variable for path?
-##TODO:  How does this work for monorepo?
-##TODO:  
-
 #!/bin/bash
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 branchname=$2
 policyrepo=$3
 
@@ -11,7 +21,7 @@ tf_validator_path="./terraform-validator-linux-amd64"
 
 ##functions
 tf_apply() {
-  terraform apply -input=false -auto-approve $branchname.tfplan || exit 1
+  terraform apply -input=false -auto-approve "${branchname}.tfplan" || exit 1
 }
 
 tf_init() {
@@ -19,7 +29,7 @@ tf_init() {
 }
 
 tf_plan() {
-  terraform plan -input=false -out $branchname.tfplan || exit 21
+  terraform plan -input=false -out "${branchname}.tfplan" || exit 21
 }
 
 tf_validate() {
@@ -27,72 +37,73 @@ tf_validate() {
     echo "terraform-validator not found!  Check path or visit"
     echo "https://github.com/forseti-security/policy-library/blob/master/docs/user_guide.md#how-to-use-terraform-validator"
   else
-    terraform show -json $branchname.tfplan > $branchname.json || exit 32
-    terraform-validator-linux-amd64 validate $branchname.json --policy-path=${policyrepo} || exit 33
+    terraform show -json "${branchname}.tfplan" > "${branchname}.json" || exit 32
+    terraform-validator-linux-amd64 validate "${branchname}.json" --policy-path="${policyrepo}" || exit 33
   fi
 }
 
 ##main
 case "$1" in
-  apply ) 
+  apply )
     echo "*************** TERRAFORM APPLY *******************"
     echo "      At environment: ${branchname} "
     echo "***************************************************"
-    
-    cd ./envs/$branchname
-    
-    if [ ! -d ".terraform" ]; then 
+
+    cd "./envs/${branchname}" || exit 44
+
+    if [ ! -d ".terraform" ]; then
       tf_init
     fi
-    
+
     if [ ! -f "${branchname}.tfplan" ]; then
       tf_plan
     fi
-    
+
     tf_validate
-    
+
     tf_apply
     ;;
 
-  init ) 
+  init )
     echo "*************** TERRAFORM INIT *******************"
     echo "      At environment: ${branchname} "
     echo "**************************************************"
-    
-    cd ./envs/$branchname
-    
+
+    cd "./envs/${branchname}" || exit 44
+
     tf_init
     ;;
 
-  plan ) 
+  plan )
     echo "*************** TERRAFORM PLAN *******************"
     echo "      At environment: ${branchname} "
     echo "**************************************************"
-    
-    cd ./envs/$branchname
-    if [ ! -d ".terraform" ]; then 
+
+    cd "./envs/${branchname}" || exit 44
+
+    if [ ! -d ".terraform" ]; then
       tf_init
     fi
-    
+
     tf_plan
     ;;
-  
+
   validate )
     echo "*************** TERRAFORM VALIDATE ******************"
     echo "      At environment: ${branchname} "
     echo "      Using policy from: ${policyrepo} "
     echo "****************************************************"
-    
-    cd ./envs/$branchname
-    
-    if [ ! -d ".terraform" ]; then 
+
+    cd "./envs/${branchname}" || exit 44
+
+    if [ ! -d ".terraform" ]; then
       tf_init
     fi
-    
+
     if [ ! -f "${branchname}.tfplan" ]; then
       tf_plan
     fi
-    
+
     tf_validate
     ;;
   * )

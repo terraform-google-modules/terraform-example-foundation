0bb69f51695e1241d1cc567d3f1935bc7da7d5dc
Atos Ferreira Machado <atos@ciandt.com>
2020-07-23 15:33:27 -0300

    Adds bootstrap tests

--- test/integration/bootstrap/controls/bootstrap.rb
+++ test/integration/bootstrap/controls/bootstrap.rb
@@ -11,3 +11,72 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+org_id = attribute('org_id')
+seed_project_id = attribute('seed_project_id')
+gcs_bucket_tfstate = attribute('gcs_bucket_tfstate')
+terraform_sa_email = attribute('terraform_sa_email')
+
+activate_apis = [
+  'serviceusage.googleapis.com',
+  'servicenetworking.googleapis.com',
+  'compute.googleapis.com',
+  'logging.googleapis.com',
+  'bigquery.googleapis.com',
+  'cloudresourcemanager.googleapis.com',
+  'cloudbilling.googleapis.com',
+  'iam.googleapis.com',
+  'admin.googleapis.com',
+  'appengine.googleapis.com',
+  'storage-api.googleapis.com',
+  'monitoring.googleapis.com',
+  'pubsub.googleapis.com',
+  'securitycenter.googleapis.com',
+  'accesscontextmanager.googleapis.com'
+]
+
+sa_org_iam_permissions = [
+  'roles/accesscontextmanager.policyAdmin',
+  'roles/billing.user',
+  'roles/compute.networkAdmin',
+  'roles/compute.xpnAdmin',
+  'roles/iam.securityAdmin',
+  'roles/iam.serviceAccountAdmin',
+  'roles/logging.configWriter',
+  'roles/orgpolicy.policyAdmin',
+  'roles/resourcemanager.folderAdmin',
+  'roles/securitycenter.notificationConfigEditor',
+  'roles/resourcemanager.organizationViewer'
+]
+
+org_name = "organizations/#{org_id}"
+
+control 'bootstrap' do
+  title 'Bootstrap module GCP resources'
+
+  describe google_project(project: seed_project_id) do
+    it { should exist }
+  end
+
+  describe google_storage_bucket(name: gcs_bucket_tfstate) do
+    it { should exist }
+  end
+
+  describe google_service_account(project: seed_project_id, name: terraform_sa_email) do
+    it { should exist }
+  end
+
+  sa_org_iam_permissions.each do |role|
+    describe google_organization_iam_binding(name: org_name, role: role) do
+      it { should exist }
+      its('members') { should include "serviceAccount:#{terraform_sa_email}" }
+    end
+  end
+
+  activate_apis.each do |api|
+    describe google_project_service(project: seed_project_id, name: api) do
+      it { should exist }
+      its('state') { should cmp 'ENABLED' }
+    end
+  end
+end

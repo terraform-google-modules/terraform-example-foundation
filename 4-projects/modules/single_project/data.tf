91b4531c026422b181f8a557c3e05b79d0fb30bc
bharathkkb <bharathkrishnakb@gmail.com>
2020-07-24 22:51:06 -0500

    fmt

--- 4-projects/modules/single_project/data.tf
+++ 4-projects/modules/single_project/data.tf
@@ -15,12 +15,12 @@
  */
 
 data "google_projects" "projects" {
-  count = var.vpc_type == "" ? 0 : 1
+  count  = var.vpc_type == "" ? 0 : 1
   filter = "labels.application_name=${var.vpc_type}-shared-vpc-host-${var.environment} lifecycleState=ACTIVE"
 }
 
 data "google_compute_network" "shared_vpc" {
-  count = var.vpc_type == "" ? 0 : 1
+  count   = var.vpc_type == "" ? 0 : 1
   name    = "vpc-${var.env_code}-shared-${var.vpc_type}"
   project = data.google_projects.projects[0].projects[0].project_id
 }

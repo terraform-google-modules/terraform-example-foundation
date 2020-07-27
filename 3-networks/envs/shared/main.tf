9c98dd4d868268452cb471a1ba8a38b8cb6af40d
daniel-cit <55760939+daniel-cit@users.noreply.github.com>
2020-07-27 19:27:21 -0300

    fix: DNS hub default regions and org id for tfvars (#140)
    * make default region variables fro DNS hub consistent with the other environments
    
    * add org_id to terraform.example.tfvars

--- 3-networks/envs/shared/main.tf
+++ 3-networks/envs/shared/main.tf
@@ -47,16 +47,16 @@ module "dns_hub_vpc" {
   delete_default_internet_gateway_routes = "true"
 
   subnets = [{
-    subnet_name           = "sb-dns-hub-${var.dns_default_region1}"
+    subnet_name           = "sb-dns-hub-${var.default_region1}"
     subnet_ip             = "172.16.0.0/25"
-    subnet_region         = var.dns_default_region1
+    subnet_region         = var.default_region1
     subnet_private_access = "true"
     subnet_flow_logs      = var.subnetworks_enable_logging
     description           = "DNS hub subnet for region 1."
     }, {
-    subnet_name           = "sb-dns-hub-${var.dns_default_region2}"
+    subnet_name           = "sb-dns-hub-${var.default_region2}"
     subnet_ip             = "172.16.0.128/25"
-    subnet_region         = var.dns_default_region2
+    subnet_region         = var.default_region2
     subnet_private_access = "true"
     subnet_flow_logs      = var.subnetworks_enable_logging
     description           = "DNS hub subnet for region 2."
@@ -111,10 +111,10 @@ module "dns-forwarding-zone" {
 module "dns_hub_region1_router1" {
   source  = "terraform-google-modules/cloud-router/google"
   version = "~> 0.2.0"
-  name    = "cr-dns-hub-${var.dns_default_region1}-cr1"
+  name    = "cr-dns-hub-${var.default_region1}-cr1"
   project = local.dns_hub_project_id
   network = module.dns_hub_vpc.network_name
-  region  = var.dns_default_region1
+  region  = var.default_region1
   bgp = {
     asn                  = var.bgp_asn_dns
     advertised_ip_ranges = [{ range = "35.199.192.0/19" }]
@@ -124,10 +124,10 @@ module "dns_hub_region1_router1" {
 module "dns_hub_region1_router2" {
   source  = "terraform-google-modules/cloud-router/google"
   version = "~> 0.2.0"
-  name    = "cr-dns-hub-${var.dns_default_region1}-cr2"
+  name    = "cr-dns-hub-${var.default_region1}-cr2"
   project = local.dns_hub_project_id
   network = module.dns_hub_vpc.network_name
-  region  = var.dns_default_region1
+  region  = var.default_region1
   bgp = {
     asn                  = var.bgp_asn_dns
     advertised_ip_ranges = [{ range = "35.199.192.0/19" }]
@@ -137,10 +137,10 @@ module "dns_hub_region1_router2" {
 module "dns_hub_region2_router1" {
   source  = "terraform-google-modules/cloud-router/google"
   version = "~> 0.2.0"
-  name    = "cr-dns-hub-${var.dns_default_region2}-cr3"
+  name    = "cr-dns-hub-${var.default_region2}-cr3"
   project = local.dns_hub_project_id
   network = module.dns_hub_vpc.network_name
-  region  = var.dns_default_region2
+  region  = var.default_region2
   bgp = {
     asn                  = var.bgp_asn_dns
     advertised_ip_ranges = [{ range = "35.199.192.0/19" }]
@@ -150,10 +150,10 @@ module "dns_hub_region2_router1" {
 module "dns_hub_region2_router2" {
   source  = "terraform-google-modules/cloud-router/google"
   version = "~> 0.2.0"
-  name    = "cr-dns-hub-${var.dns_default_region2}-cr4"
+  name    = "cr-dns-hub-${var.default_region2}-cr4"
   project = local.dns_hub_project_id
   network = module.dns_hub_vpc.network_name
-  region  = var.dns_default_region2
+  region  = var.default_region2
   bgp = {
     asn                  = var.bgp_asn_dns
     advertised_ip_ranges = [{ range = "35.199.192.0/19" }]
@@ -172,7 +172,7 @@ module "dns_hub_region2_router2" {
 
 #   vpc_name = "dns-hub"
 
-#   region1                        = var.dns_default_region1
+#   region1                        = var.default_region1
 #   region1_router1_name           = module.dns_hub_region1_router1.router.name
 #   region1_interconnect1          = "https://www.googleapis.com/compute/v1/projects/example-interconnect-project/global/interconnects/example-interconnect-1"
 #   region1_interconnect1_location = "las-zone1-770"
@@ -180,11 +180,11 @@ module "dns_hub_region2_router2" {
 #   region1_interconnect2          = "https://www.googleapis.com/compute/v1/projects/example-interconnect-project/global/interconnects/example-interconnect-2"
 #   region1_interconnect2_location = "las-zone1-770"
 
-#   region2                        = var.dns_default_region2
+#   region2                        = var.default_region2
 #   region2_router1_name           = module.dns_hub_region2_router1.router.name
 #   region2_interconnect1          = "https://www.googleapis.com/compute/v1/projects/example-interconnect-project/global/interconnects/example-interconnect-3"
 #   region2_interconnect1_location = "lax-zone2-19"
-#   region2_router2_name           = module.dns_hub_region2_router2.router.name
+#   region2_router2_name           = module.hub_region2_router2.router.name
 #   region2_interconnect2          = "https://www.googleapis.com/compute/v1/projects/example-interconnect-project/global/interconnects/example-interconnect-4"
 #   region2_interconnect2_location = "lax-zone1-403"
 
--- 3-networks/envs/shared/terraform.example.tfvars
+++ 3-networks/envs/shared/terraform.example.tfvars
@@ -14,11 +14,13 @@
  * limitations under the License.
  */
 
+org_id = "000000000000"
+
 terraform_service_account = "org-terraform@example-project-2334.iam.gserviceaccount.com"
 
-dns_default_region1 = "us-central1"
+default_region1 = "us-central1"
 
-dns_default_region2 = "us-west1"
+default_region2 = "us-west1"
 
 domain = "example.com."
 

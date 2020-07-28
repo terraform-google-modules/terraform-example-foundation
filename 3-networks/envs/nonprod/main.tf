91939ea690616f9d2e8cc92cd4206b431165f4bf
daniel-cit <55760939+daniel-cit@users.noreply.github.com>
2020-07-28 00:35:40 -0300

    fix: naming of fw rules, disable NAT and FW rules by default (#139)

--- 3-networks/envs/nonprod/main.tf
+++ 3-networks/envs/nonprod/main.tf
@@ -133,18 +133,17 @@ module "restricted_shared_vpc" {
 *****************************************/
 
 module "private_shared_vpc" {
-  source                     = "../../modules/standard_shared_vpc"
-  project_id                 = local.private_project_id
-  environment_code           = local.environment_code
-  vpc_label                  = "private"
-  private_service_cidr       = "10.0.80.0/20"
-  nat_enabled                = true
-  nat_bgp_asn                = "64514"
-  default_region1            = var.default_region1
-  default_region2            = var.default_region2
-  domain                     = var.domain
-  bgp_asn_subnet             = "64514"
-  windows_activation_enabled = true
+  source               = "../../modules/standard_shared_vpc"
+  project_id           = local.private_project_id
+  environment_code     = local.environment_code
+  vpc_label            = "private"
+  private_service_cidr = "10.0.80.0/20"
+  nat_enabled          = false
+  nat_bgp_asn          = "64514"
+  default_region1      = var.default_region1
+  default_region2      = var.default_region2
+  domain               = var.domain
+  bgp_asn_subnet       = "64514"
 
   subnets = [
     {

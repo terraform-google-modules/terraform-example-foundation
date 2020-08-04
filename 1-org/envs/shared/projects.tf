0416cae7da8098aefd9e1dd5bc4927dbeec211e1
Daniel da Silva Andrade <dandrade@ciandt.com>
2020-08-04 10:18:48 -0300

    remove org from project names

--- 1-org/envs/shared/projects.tf
+++ 1-org/envs/shared/projects.tf
@@ -43,7 +43,7 @@ module "org_billing_logs" {
   random_project_id           = "true"
   impersonate_service_account = var.terraform_service_account
   default_service_account     = "depriviledge"
-  name                        = "prj-c-org-billing-logs"
+  name                        = "prj-c-billing-logs"
   org_id                      = var.org_id
   billing_account             = var.billing_account
   folder_id                   = google_folder.common.id
@@ -66,7 +66,7 @@ module "org_secrets" {
   random_project_id           = "true"
   impersonate_service_account = var.terraform_service_account
   default_service_account     = "depriviledge"
-  name                        = "prj-c-org-secrets"
+  name                        = "prj-c-secrets"
   org_id                      = var.org_id
   billing_account             = var.billing_account
   folder_id                   = google_folder.common.id
@@ -89,7 +89,7 @@ module "interconnect" {
   random_project_id           = "true"
   impersonate_service_account = var.terraform_service_account
   default_service_account     = "depriviledge"
-  name                        = "prj-c-org-interconnect"
+  name                        = "prj-c-interconnect"
   org_id                      = var.org_id
   billing_account             = var.billing_account
   folder_id                   = google_folder.common.id

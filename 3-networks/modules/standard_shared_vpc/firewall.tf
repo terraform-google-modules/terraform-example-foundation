115758696c851bc52e2035ba3c84295419185fcb
Daniel da Silva Andrade <dandrade@ciandt.com>
2020-08-04 01:35:39 -0300

    update tags

--- 3-networks/modules/standard_shared_vpc/firewall.tf
+++ 3-networks/modules/standard_shared_vpc/firewall.tf
@@ -38,7 +38,7 @@ resource "google_compute_firewall" "deny_all_egress" {
 
 
 resource "google_compute_firewall" "allow_private_api_egress" {
-  name           = "fw-${var.environment_code}-shared-private-65534-e-a-allow-google-api-all-tcp-443"
+  name           = "fw-${var.environment_code}-shared-private-65534-e-a-allow-google-apis-all-tcp-443"
   network        = module.main.network_name
   project        = var.project_id
   direction      = "EGRESS"
@@ -52,7 +52,7 @@ resource "google_compute_firewall" "allow_private_api_egress" {
 
   destination_ranges = [local.private_googleapis_cidr]
 
-  target_tags = ["allow-google-api"]
+  target_tags = ["allow-google-apis"]
 }
 
 
@@ -101,7 +101,7 @@ resource "google_compute_firewall" "allow_iap_rdp" {
 // Allow access to kms.windows.googlecloud.com for Windows license activation
 resource "google_compute_firewall" "allow_windows_activation" {
   count          = var.windows_activation_enabled ? 1 : 0
-  name           = "fw-${var.environment_code}-shared-private-0-e-a-allow-win-all-tcp-1688"
+  name           = "fw-${var.environment_code}-shared-private-0-e-a-allow-win-activation-all-tcp-1688"
   network        = module.main.network_name
   project        = var.project_id
   direction      = "EGRESS"
@@ -115,7 +115,7 @@ resource "google_compute_firewall" "allow_windows_activation" {
 
   destination_ranges = ["35.190.247.13/32"]
 
-  target_tags = ["allow-win"]
+  target_tags = ["allow-win-activation"]
 }
 
 // Allow traffic for Internal & Global load balancing health check and load balancing IP ranges.

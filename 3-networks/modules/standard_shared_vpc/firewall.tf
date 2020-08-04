155a3a1818911b2b6e2cf9bb9ee1744afeddda21
daniel-cit <55760939+daniel-cit@users.noreply.github.com>
2020-08-04 03:00:32 -0300

    fix: add missing tags to firewall rules. (#178)

--- 3-networks/modules/standard_shared_vpc/firewall.tf
+++ 3-networks/modules/standard_shared_vpc/firewall.tf
@@ -38,7 +38,7 @@ resource "google_compute_firewall" "deny_all_egress" {
 
 
 resource "google_compute_firewall" "allow_private_api_egress" {
-  name           = "fw-${var.environment_code}-shared-private-65534-e-a-all-all-tcp-443"
+  name           = "fw-${var.environment_code}-shared-private-65534-e-a-allow-google-apis-all-tcp-443"
   network        = module.main.network_name
   project        = var.project_id
   direction      = "EGRESS"
@@ -51,6 +51,8 @@ resource "google_compute_firewall" "allow_private_api_egress" {
   }
 
   destination_ranges = [local.private_googleapis_cidr]
+
+  target_tags = ["allow-google-apis"]
 }
 
 
@@ -99,7 +101,7 @@ resource "google_compute_firewall" "allow_iap_rdp" {
 // Allow access to kms.windows.googlecloud.com for Windows license activation
 resource "google_compute_firewall" "allow_windows_activation" {
   count          = var.windows_activation_enabled ? 1 : 0
-  name           = "fw-${var.environment_code}-shared-private-0-e-a-all-tcp-1688"
+  name           = "fw-${var.environment_code}-shared-private-0-e-a-allow-win-activation-all-tcp-1688"
   network        = module.main.network_name
   project        = var.project_id
   direction      = "EGRESS"
@@ -110,6 +112,10 @@ resource "google_compute_firewall" "allow_windows_activation" {
     protocol = "tcp"
     ports    = ["1688"]
   }
+
+  destination_ranges = ["35.190.247.13/32"]
+
+  target_tags = ["allow-win-activation"]
 }
 
 // Allow traffic for Internal & Global load balancing health check and load balancing IP ranges.

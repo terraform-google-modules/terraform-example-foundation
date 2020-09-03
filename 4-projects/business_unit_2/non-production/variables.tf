d1528b1fdb431e8c86c499deffece9c83b80e454
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2020-09-03 15:47:13 -0300

    Adds optionals section on readme

--- 4-projects/business_unit_2/non-production/variables.tf
+++ 4-projects/business_unit_2/non-production/variables.tf
@@ -50,3 +50,21 @@ variable "perimeter_name" {
   description = "Access context manager service perimeter name to attach the restricted svpc project."
   type        = string
 }
+
+variable "alert_spent_percents" {
+  description = "A list of percentages of the budget to alert on when threshold is exceeded"
+  type        = list(number)
+  default     = [0.5, 0.75, 0.9, 0.95]
+}
+
+variable "alert_pubsub_topic" {
+  description = "The name of the Cloud Pub/Sub topic where budget related messages will be published, in the form of `projects/{project_id}/topics/{topic_id}`"
+  type        = string
+  default     = null
+}
+
+variable "budget_amount" {
+  description = "The amount to use as the budget"
+  type        = number
+  default     = 1000
+}

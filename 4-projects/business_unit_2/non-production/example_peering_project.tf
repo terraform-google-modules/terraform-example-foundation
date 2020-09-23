2c797da8b3f4d09ac9254abb40588c5c0087b613
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2020-09-23 10:26:48 -0300

    Fixes code review issues

--- 4-projects/business_unit_2/non-production/example_peering_project.tf
+++ 4-projects/business_unit_2/non-production/example_peering_project.tf
@@ -14,24 +14,13 @@
  * limitations under the License.
  */
 
-locals {
-  vpc_name      = "${local.env_code}-peering-base"
-  network_name  = "vpc-${local.vpc_name}"
-  env_code      = "n"
-  environment   = "non-production"
-  vpc_type      = "base"
-  business_code = "bu2"
-}
-
 data "google_projects" "projects" {
-  count  = local.vpc_type == "" ? 0 : 1
-  filter = "parent.id:${split("/", data.google_active_folder.env.name)[1]} labels.application_name=${local.vpc_type}-shared-vpc-host labels.environment=${local.environment} lifecycleState=ACTIVE"
+  filter = "parent.id:${split("/", data.google_active_folder.env.name)[1]} labels.application_name=base-shared-vpc-host labels.environment=non-production lifecycleState=ACTIVE"
 }
 
 data "google_compute_network" "shared_vpc" {
-  count   = local.vpc_type == "" ? 0 : 1
-  name    = "vpc-${local.env_code}-shared-${local.vpc_type}"
-  project = data.google_projects.projects[0].projects[0].project_id
+  name    = "vpc-n-shared-base"
+  project = data.google_projects.projects.projects[0].project_id
 }
 
 module "peering_project" {
@@ -41,22 +30,22 @@ module "peering_project" {
   billing_account             = var.billing_account
   folder_id                   = data.google_active_folder.env.name
   skip_gcloud_download        = var.skip_gcloud_download
-  environment                 = local.environment
+  environment                 = "non-production"
 
   # Metadata
   project_prefix    = "sample-peering"
-  application_name  = "${local.business_code}-sample-peering"
+  application_name  = "bu2-sample-peering"
   billing_code      = "1234"
   primary_contact   = "example@example.com"
   secondary_contact = "example2@example.com"
-  business_code     = local.business_code
+  business_code     = "bu2"
 }
 
 module "peering_network" {
   source                                 = "terraform-google-modules/network/google"
   version                                = "~> 2.0"
   project_id                             = module.peering_project.project_id
-  network_name                           = local.network_name
+  network_name                           = "vpc-n-peering-base"
   shared_vpc_host                        = "false"
   delete_default_internet_gateway_routes = "true"
   subnets                                = []
@@ -65,7 +54,7 @@ module "peering_network" {
 module "peering" {
   source = "terraform-google-modules/network/google//modules/network-peering"
 
-  prefix        = "${local.business_code}-${local.env_code}"
+  prefix        = "bu2-n"
   local_network = module.peering_network.network_self_link
-  peer_network  = data.google_compute_network.shared_vpc[0].self_link
+  peer_network  = data.google_compute_network.shared_vpc.self_link
 }

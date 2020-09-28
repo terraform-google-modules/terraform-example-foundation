970db735b481ec4e3820e96d93bd509ca7e4c5cb
umairidris <umairidris@google.com>
2020-09-28 19:34:08 -0400

    add folder audit config

--- 1-org/envs/shared/iam.tf
+++ 1-org/envs/shared/iam.tf
@@ -18,9 +18,25 @@
   Audit Logs - IAM
 *****************************************/
 
-resource "google_organization_iam_audit_config" "config" {
-  count   = var.data_access_logs_enabled ? 1 : 0
+resource "google_organization_iam_audit_config" "org_config" {
+  count   = var.data_access_logs_enabled && var.parent_folder == "" ? 1 : 0
   org_id  = var.org_id
+  service = "allServices"
+
+  audit_log_config {
+    log_type = "DATA_READ"
+  }
+  audit_log_config {
+    log_type = "DATA_WRITE"
+  }
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+}
+
+resource "google_folder_iam_audit_config" "folder_config" {
+  count   = var.data_access_logs_enabled && var.parent_folder != "" ? 1 : 0
+  folder  = "folders/${var.parent_folder}"
   service = "allServices"
 
   audit_log_config {

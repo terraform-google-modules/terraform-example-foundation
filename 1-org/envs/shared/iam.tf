6e887e028195eabf7e630b86d529196bd33db1a4
Umair Idris <umairidris@google.com>
2020-09-28 21:40:41 -0400

    fix: enable data access logs collection (#249)

--- 1-org/envs/shared/iam.tf
+++ 1-org/envs/shared/iam.tf
@@ -18,6 +18,38 @@
   Audit Logs - IAM
 *****************************************/
 
+resource "google_organization_iam_audit_config" "org_config" {
+  count   = var.data_access_logs_enabled && var.parent_folder == "" ? 1 : 0
+  org_id  = var.org_id
+  service = "allServices"
+
+  audit_log_config {
+    log_type = "DATA_READ"
+  }
+  audit_log_config {
+    log_type = "DATA_WRITE"
+  }
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+}
+
+resource "google_folder_iam_audit_config" "folder_config" {
+  count   = var.data_access_logs_enabled && var.parent_folder != "" ? 1 : 0
+  folder  = "folders/${var.parent_folder}"
+  service = "allServices"
+
+  audit_log_config {
+    log_type = "DATA_READ"
+  }
+  audit_log_config {
+    log_type = "DATA_WRITE"
+  }
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+}
+
 resource "google_project_iam_member" "audit_log_bq_user" {
   project = module.org_audit_logs.project_id
   role    = "roles/bigquery.user"

d2dfd7a87cef4c5a74d7dd9ddfbf3ebddb0b902d
bharathkkb <bharathkrishnakb@gmail.com>
2020-10-26 13:19:04 -0500

    bump router

--- 3-networks/modules/base_shared_vpc/main.tf
+++ 3-networks/modules/base_shared_vpc/main.tf
@@ -95,7 +95,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 
 module "region1_router1" {
   source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.2.0"
+  version = "~> 0.3.0"
   name    = "cr-${local.vpc_name}-${var.default_region1}-cr1"
   project = var.project_id
   network = module.main.network_name
@@ -109,7 +109,7 @@ module "region1_router1" {
 
 module "region1_router2" {
   source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.2.0"
+  version = "~> 0.3.0"
   name    = "cr-${local.vpc_name}-${var.default_region1}-cr2"
   project = var.project_id
   network = module.main.network_name
@@ -123,7 +123,7 @@ module "region1_router2" {
 
 module "region2_router1" {
   source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.2.0"
+  version = "~> 0.3.0"
   name    = "cr-${local.vpc_name}-${var.default_region2}-cr3"
   project = var.project_id
   network = module.main.network_name
@@ -137,7 +137,7 @@ module "region2_router1" {
 
 module "region2_router2" {
   source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.2.0"
+  version = "~> 0.3.0"
   name    = "cr-${local.vpc_name}-${var.default_region2}-cr4"
   project = var.project_id
   network = module.main.network_name

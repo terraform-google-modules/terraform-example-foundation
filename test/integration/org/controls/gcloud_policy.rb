6e8fc2a0391570b90c8a1dc9bb20805a56a51d27
Daniel Andrade <dandrade@ciandt.com>
2020-11-17 10:03:53 -0300

    fix: remove shielded VM org policy (#286)

--- test/integration/org/controls/gcloud_policy.rb
+++ test/integration/org/controls/gcloud_policy.rb
@@ -22,7 +22,6 @@ boolean_policy_constraints = [
   'constraints/compute.vmExternalIpAccess',
   'constraints/compute.skipDefaultNetworkCreation',
   'constraints/compute.requireOsLogin',
-  'constraints/compute.requireShieldedVm',
   'constraints/compute.restrictXpnProjectLienRemoval',
   'constraints/sql.restrictPublicIp',
   'constraints/iam.disableServiceAccountKeyCreation',

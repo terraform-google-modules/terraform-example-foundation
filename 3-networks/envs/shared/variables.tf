d63a7f8cb9f73cc8f0246291ec27a39448942e53
Roberto Jung Drebes <drebes@google.com>
2021-03-05 05:04:54 +0100

    feat: enable hub & spoke transitivity via gateway VMs (#322)

--- 3-networks/envs/shared/variables.tf
+++ 3-networks/envs/shared/variables.tf
@@ -30,6 +30,12 @@ variable "enable_hub_and_spoke" {
   default     = false
 }
 
+variable "enable_hub_and_spoke_transitivity" {
+  description = "Enable transitivity via gateway VMs on Hub-and-Spoke architecture."
+  type        = bool
+  default     = false
+}
+
 variable "access_context_manager_policy_id" {
   type        = number
   description = "The id of the default Access Context Manager policy created in step `1-org`. Can be obtained by running `gcloud access-context-manager policies list --organization YOUR-ORGANIZATION_ID --format=\"value(name)\"`."

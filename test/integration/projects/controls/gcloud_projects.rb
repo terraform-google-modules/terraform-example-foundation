a13bd26041a00b9670ff48311aeecd88af4824e5
Chris Ingrisani <71732884+chris-ingrisani@users.noreply.github.com>
2021-03-09 15:09:10 -0500

    Update gcloud_projects.rb

--- test/integration/projects/controls/gcloud_projects.rb
+++ test/integration/projects/controls/gcloud_projects.rb
@@ -197,18 +197,6 @@ control 'gcloud-projects' do
         end
       end
 
-        describe "#{base_project_sa[environment_code][business_unit]} IAM Policy" do
-          it 'should exist' do
-            expect(data).to_not be_empty
-          end
-          it "#{base_project_sa[environment_code][business_unit]} Base project SA should have the Editor role" do
-            expect(data['bindings'][0]['members']).to include(
-              "serviceAccount:#{base_project_sa[environment_code][business_unit]}"
-            )
-            expect(data['bindings'][0]['role']).to eq "roles/editor"
-          end
-        end
-
       describe command("gcloud compute shared-vpc get-host-project #{restricted_projects_id[environment_code][business_unit]} --format=json") do
         its(:exit_status) { should eq 0 }
         its(:stderr) { should eq '' }

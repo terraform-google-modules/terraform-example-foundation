4c75822991b0f617145b74930e6c5f5aa3c6b933
Chris Ingrisani <71732884+chris-ingrisani@users.noreply.github.com>
2021-03-09 14:36:58 -0500

    Update main.tf

--- 3-networks/envs/development/main.tf
+++ 3-networks/envs/development/main.tf
@@ -168,7 +168,7 @@ module "base_shared_vpc" {
 }
 
 resource "google_compute_address" "external_ip_for_http_load_balancing" {
-  name         = var.name
+  name         = var.address_name
   project      = local.base_project_id
   address_type = var.address_type
   description  = var.description

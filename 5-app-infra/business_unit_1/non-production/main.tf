a720d71706118b481fc2f97ab8465ddc052c5121
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2021-03-19 15:04:16 -0300

    Adds non-prodcution environment for bu1 example

---
+++ 5-app-infra/business_unit_1/non-production/main.tf
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+data "google_active_folder" "env" {
+  display_name = "${var.folder_prefix}-non-production"
+  parent       = var.parent_folder != "" ? "folders/${var.parent_folder}" : "organizations/${var.org_id}"
+}
+
+module "base_shared_gce_instance" {
+  source         = "../../modules/env_base"
+  environment    = "non-production"
+  vpc_type       = "base"
+  num_instances  = 1
+  folder_id      = data.google_active_folder.env.name
+  business_code  = "bu1"
+  project_suffix = "sample-base"
+  region         = "us-east1"
+  static_ips     = var.static_ips
+}

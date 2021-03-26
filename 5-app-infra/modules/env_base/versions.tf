1c6741ae789caed631ae3ac534d42676383b1e72
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2021-03-26 10:39:10 -0300

    Changes impersionate configuration to native

--- 5-app-infra/modules/env_base/versions.tf
+++ 5-app-infra/modules/env_base/versions.tf
@@ -19,7 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50"
+      version = ">= 3.61"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 3.61"
     }
     null = {
       source  = "hashicorp/null"

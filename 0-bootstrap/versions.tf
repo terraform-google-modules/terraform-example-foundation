de0a0ea85b03173c3440ea1814806d2451cc4f5f
Bharath KKB <bharathkrishnakb@gmail.com>
2021-05-11 20:55:06 -0500

    Update versions.tf

--- 0-bootstrap/versions.tf
+++ 0-bootstrap/versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50"
+      version = ">= 3.51"
     }
     null = {
       source  = "hashicorp/null"

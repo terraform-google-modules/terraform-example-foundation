521cd968bb730bc6d67c5ac543843dff16450803
William Tsoi <william@williamtsoi.net>
2021-09-14 16:49:21 +0800

    upgrading cloud router module version

--- 3-networks/modules/restricted_shared_vpc/main.tf
+++ 3-networks/modules/restricted_shared_vpc/main.tf
@@ -132,7 +132,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 
 module "region1_router1" {
   source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.3.0"
+  version = "~> 0.4.0"
   count   = var.mode != "spoke" ? 1 : 0
   name    = "cr-${local.vpc_name}-${var.default_region1}-cr5"
   project = var.project_id
@@ -147,7 +147,7 @@ module "region1_router1" {
 
 module "region1_router2" {
   source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.3.0"
+  version = "~> 0.4.0"
   count   = var.mode != "spoke" ? 1 : 0
   name    = "cr-${local.vpc_name}-${var.default_region1}-cr6"
   project = var.project_id
@@ -162,7 +162,7 @@ module "region1_router2" {
 
 module "region2_router1" {
   source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.3.0"
+  version = "~> 0.4.0"
   count   = var.mode != "spoke" ? 1 : 0
   name    = "cr-${local.vpc_name}-${var.default_region2}-cr7"
   project = var.project_id
@@ -177,7 +177,7 @@ module "region2_router1" {
 
 module "region2_router2" {
   source  = "terraform-google-modules/cloud-router/google"
-  version = "~> 0.3.0"
+  version = "~> 0.4.0"
   count   = var.mode != "spoke" ? 1 : 0
   name    = "cr-${local.vpc_name}-${var.default_region2}-cr8"
   project = var.project_id

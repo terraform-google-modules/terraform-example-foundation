05b96011944f65c61e66778e49817b09bae903cf
Luiz Dutra <luizdutra@ciandt.com>
2022-03-30 14:22:27 -0300

    fix the test

--- 3-networks/envs/non-production/main.tf
+++ 3-networks/envs/non-production/main.tf
@@ -17,8 +17,8 @@
 locals {
   env              = "non-production"
   environment_code = substr(local.env, 0, 1)
-  default_region1  = "us-central1"
-  default_region2  = "us-west1"
+  default_region1  = "us-west1"
+  default_region2  = "us-central1"
   /*
    * Base network ranges
    */

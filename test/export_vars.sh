34de162deadf08c9c56c4e42b52607fcf22cd9f7
Daniel da Silva Andrade <dandrade@ciandt.com>
2022-04-11 11:31:37 -0300

    add export to export of previous terraform script outputs

---
+++ test/export_vars.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+function export_access_context_manager_policy(){
+    # export Access Context Manager Policy ID
+    local policy_id=$(gcloud access-context-manager policies list --organization="${TF_VAR_org_id:?}" --format="value(name)")
+    export TF_VAR_access_context_manager_policy_id=$policy_id
+}
+
+function org(){
+    # TBD
+}
+
+function envs(){
+    # TBD
+}
+
+function networks(){
+    # export Access Context Manager Policy ID
+    export_access_context_manager_policy
+}
+
+function projects(){
+    # export Access Context Manager Policy ID
+    export_access_context_manager_policy
+
+    # development
+    pushd /workspace/3-networks/envs/development
+    # shellcheck disable=SC1090
+    source <(python /usr/local/bin/export_tf_outputs.py --path=/workspace/3-networks/envs/development)
+    local perimeter_name_d=$TF_VAR_restricted_service_perimeter_name
+    export TF_VAR_dev_restricted_service_perimeter_name=$perimeter_name_d
+    popd
+
+    # non-production
+    pushd /workspace/3-networks/envs/non-production
+    # shellcheck disable=SC1090
+    source <(python /usr/local/bin/export_tf_outputs.py --path=/workspace/3-networks/envs/non-production)
+    local perimeter_name_n=$TF_VAR_restricted_service_perimeter_name
+    export TF_VAR_nonprod_restricted_service_perimeter_name=$perimeter_name_n
+    popd
+
+    # production
+    pushd /workspace/3-networks/envs/production
+    # shellcheck disable=SC1090
+    source <(python /usr/local/bin/export_tf_outputs.py --path=/workspace/3-networks/envs/production)
+    local perimeter_name_p=$TF_VAR_restricted_service_perimeter_name
+    export TF_VAR_prod_restricted_service_perimeter_name=$perimeter_name_p
+    popd
+
+}
+
+function appinfra(){
+    # TBD
+}
+
+
+# parse args
+for arg in "$@"
+do
+  case $arg in
+    -n|--networks)
+      networks
+      shift
+      ;;
+    -o|--org)
+      org
+      shift
+      ;;
+    -e|--envs)
+      envs
+      shift
+      ;;
+    -a|--appinfra)
+      appinfra
+      shift
+      ;;
+    -p|--projects)
+      projects
+      shift
+      ;;
+      *) # end argument parsing
+      shift
+      ;;
+  esac
+done
--- test/integration/projects/projects_test.go
+++ test/integration/projects/projects_test.go
@@ -21,9 +21,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type SharedData struct {
+	string CloudbuildSA
+}
+
 func TestProjects(t *testing.T) {
 
-	for _, tt := range []struct {
+	var sharedData = map[string]*SharedData{
+		"bu1_shared": {
+			CloudbuildSA: "",
+		},
+		"bu2_shared": {
+			CloudbuildSA: "",
+		},
+	}
+
+	for _, tts := range []struct {
 		name  string
 		tfDir string
 	}{
@@ -32,37 +45,75 @@ func TestProjects(t *testing.T) {
 			tfDir: "../../../4-projects/business_unit_1/shared",
 		},
 		{
+			name: "bu2_shared",
+			tfDir: "../../../4-projects/business_unit_2/shared",
+		},
+
+	}{
+		t.Run(tts.name, func(t *testing.T) {
+			shared := tft.NewTFBlueprintTest(t,
+				tft.WithTFDir(tts.tfDir),
+			)
+            shared.DefineApply(
+				func(assert *assert.Assertions) {
+					// perform default verification ensuring Terraform reports no additional changes on an applied blueprint
+					shared.DefaultApply(assert)
+					sharedData[tts.name].CloudbuildSA = shared.GetStringOutput("cloudbuild_sa")
+				})
+			shared.DefineVerify(
+				func(assert *assert.Assertions) {
+					// perform default verification ensuring Terraform reports no additional changes on an applied blueprint
+					shared.DefaultVerify(assert)
+					sharedData[tts.name].CloudbuildSA = shared.GetStringOutput("cloudbuild_sa")
+				})
+			shared.Test()
+		})
+
+	}
+
+	for _, tt := range []struct {
+		name  string
+		sharedData string
+		tfDir string
+	}{
+		{
 			name: "bu1_development",
+			sharedData: "bu1_shared",
 			tfDir: "../../../4-projects/business_unit_1/development",
 		},
 		{
 			name: "bu1_non-production",
+			sharedData: "bu1_shared",
 			tfDir: "../../../4-projects/business_unit_1/non-production",
 		},
 		{
 			name: "bu1_production",
+			sharedData: "bu1_shared",
 			tfDir: "../../../4-projects/business_unit_1/production",
 		},
 		{
-			name: "bu2_shared",
-			tfDir: "../../../4-projects/business_unit_2/shared",
-		},
-		{
 			name: "bu2_development",
+			sharedData: "bu2_shared",
 			tfDir: "../../../4-projects/business_unit_2/development",
 		},
 		{
 			name: "bu2_non-production",
+			sharedData: "bu2_shared",
 			tfDir: "../../../4-projects/business_unit_2/non-production",
 		},
 		{
 			name: "bu2_production",
+			sharedData: "bu2_shared",
 			tfDir: "../../../4-projects/business_unit_2/production",
 		},
 	}{
 		t.Run(tt.name, func(t *testing.T) {
+			vars := map[string]string{
+				"app_infra_pipeline_cloudbuild_sa": sharedData[tt.sharedData].CloudbuildSA,
+			}
 			projects := tft.NewTFBlueprintTest(t,
 				tft.WithTFDir(tt.tfDir),
+				tft.WithVars(vars),
 			)
 			projects.DefineVerify(
 				func(assert *assert.Assertions) {

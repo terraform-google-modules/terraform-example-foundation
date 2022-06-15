ccfc0171ee9489cd6567edfe6fa6be03a8048bfb
Iris Chen <ciris@google.com>
2022-06-14 23:05:18 -0400

    use git branch for bootstrap module

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -31,7 +31,7 @@ resource "google_folder" "bootstrap" {
 
 module "seed_bootstrap" {
   source                         = "terraform-google-modules/bootstrap/google"
-  version                        = "~> master"
+  version                        = "~> 5.0"
   org_id                         = var.org_id
   folder_id                      = google_folder.bootstrap.id
   project_id                     = "${var.project_prefix}-b-seed"
@@ -104,8 +104,7 @@ resource "google_billing_account_iam_member" "tf_billing_admin" {
 
 // Comment-out the cloudbuild_bootstrap module and its outputs if you want to use Jenkins instead of Cloud Build
 module "cloudbuild_bootstrap" {
-  source                      = "terraform-google-modules/bootstrap/google//modules/cloudbuild"
-  version                     = "~> master"
+  source                      = "github.com/terraform-google-modules/terraform-google-bootstrap//modules/cloudbuild"
   org_id                      = var.org_id
   folder_id                   = google_folder.bootstrap.id
   project_id                  = "${var.project_prefix}-b-cicd"

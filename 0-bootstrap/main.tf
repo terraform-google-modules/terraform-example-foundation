016a0fae4d75f5680bd4679b21b29451d88f103d
Iris Chen <ciris@google.com>
2022-06-21 11:49:45 -0400

    Fix mismatch module

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -30,7 +30,7 @@ resource "google_folder" "bootstrap" {
 }
 
 module "seed_bootstrap" {
-  source                         = "github.com/terraform-google-modules/terraform-google-bootstrap/modules/cloudbuild"
+  source                         = "github.com/terraform-google-modules/terraform-google-bootstrap"
   org_id                         = var.org_id
   folder_id                      = google_folder.bootstrap.id
   project_id                     = "${var.project_prefix}-b-seed"
@@ -103,7 +103,7 @@ resource "google_billing_account_iam_member" "tf_billing_admin" {
 
 // Comment-out the cloudbuild_bootstrap module and its outputs if you want to use Jenkins instead of Cloud Build
 module "cloudbuild_bootstrap" {
-  source                      = "github.com/terraform-google-modules/terraform-google-bootstrap/modules/cloudbuild"
+  source                      = "github.com/terraform-google-modules/terraform-google-bootstrap//modules/cloudbuild"
   org_id                      = var.org_id
   folder_id                   = google_folder.bootstrap.id
   project_id                  = "${var.project_prefix}-b-cicd"

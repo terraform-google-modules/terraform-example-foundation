9b2b3c2a2c6368c0e5512d748d3a904cb6e82e20
Iris Chen <ciris@google.com>
2022-06-23 10:07:48 -0400

    move bootstrap to use 6.0

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -30,7 +30,8 @@ resource "google_folder" "bootstrap" {
 }
 
 module "seed_bootstrap" {
-  source                         = "github.com/terraform-google-modules/terraform-google-bootstrap"
+  source                         = "terraform-google-modules/bootstrap/google"
+  version                        = "~> 6.0"
   org_id                         = var.org_id
   folder_id                      = google_folder.bootstrap.id
   project_id                     = "${var.project_prefix}-b-seed"
@@ -103,7 +104,8 @@ resource "google_billing_account_iam_member" "tf_billing_admin" {
 
 // Comment-out the cloudbuild_bootstrap module and its outputs if you want to use Jenkins instead of Cloud Build
 module "cloudbuild_bootstrap" {
-  source                      = "github.com/terraform-google-modules/terraform-google-bootstrap//modules/cloudbuild"
+  source                      = "terraform-google-modules/bootstrap/google//modules/cloudbuild"
+  version                     = "~> 6.0"
   org_id                      = var.org_id
   folder_id                   = google_folder.bootstrap.id
   project_id                  = "${var.project_prefix}-b-cicd"

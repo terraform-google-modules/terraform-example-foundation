38b4365fa381df27029253d5c98cea53c01ee59d
Daniel da Silva Andrade <dandrade@ciandt.com>
2022-08-02 09:51:14 -0300

    pin version of google provider to 4.30.0 to prevent issue in bucket naming validation

--- 4-projects/modules/infra_pipelines/versions.tf
+++ 4-projects/modules/infra_pipelines/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50"
+      version = "~> 4.30.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.50"
+      version = "~> 4.30.0"
     }
     null = {
       source = "hashicorp/null"

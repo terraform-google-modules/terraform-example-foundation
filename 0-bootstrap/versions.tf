7fde86957d08897cd953d3b2ba970a694aa3a0d4
Daniel da Silva Andrade <dandrade@ciandt.com>
2022-08-05 14:51:37 -0300

    exclude version 4.31.0 from provider options

--- 0-bootstrap/versions.tf
+++ 0-bootstrap/versions.tf
@@ -18,8 +18,9 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     google = {
+      // version 4.31.0 removed because of issue https://github.com/hashicorp/terraform-provider-google/issues/12226
       source  = "hashicorp/google"
-      version = ">= 3.50"
+      version = ">= 3.50, != 4.31.0"
     }
   }
 

17e264acc0ec5188787f799bcebc652652bae523
Mauro <maurosj@ciandt.com>
2022-08-26 15:32:41 -0300

    Adding minor version to pvt svc connect module

--- 3-networks-dual-svpc/modules/restricted_shared_vpc/private_service_connect.tf
+++ 3-networks-dual-svpc/modules/restricted_shared_vpc/private_service_connect.tf
@@ -17,6 +17,7 @@
 
 module "private_service_connect" {
   source                     = "terraform-google-modules/network/google//modules/private-service-connect"
+  version                    = "~> 5.2"
   project_id                 = var.project_id
   dns_code                   = "dz-${var.environment_code}-shared-restricted"
   network_self_link          = module.main.network_self_link

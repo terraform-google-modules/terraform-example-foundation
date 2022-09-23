5e114d5b1a5c454b893f75e6febfcbc4916d8d83
Daniel da Silva Andrade <dandrade@ciandt.com>
2022-09-23 13:49:46 -0300

    read projects backend bucket from bootstrap remote state

--- 4-projects/modules/base_env/main.tf
+++ 4-projects/modules/base_env/main.tf
@@ -22,6 +22,7 @@ locals {
   default_region                      = data.terraform_remote_state.bootstrap.outputs.common_config.default_region
   project_prefix                      = data.terraform_remote_state.bootstrap.outputs.common_config.project_prefix
   folder_prefix                       = data.terraform_remote_state.bootstrap.outputs.common_config.folder_prefix
+  projects_backend_bucket             = data.terraform_remote_state.bootstrap.outputs.projects_gcs_bucket_tfstate
   perimeter_name                      = data.terraform_remote_state.network_env.outputs.restricted_service_perimeter_name
   base_network_self_link              = data.terraform_remote_state.network_env.outputs.base_network_self_link
   base_subnets_self_links             = data.terraform_remote_state.network_env.outputs.base_subnets_self_links
@@ -64,7 +65,7 @@ data "terraform_remote_state" "business_unit_shared" {
   backend = "gcs"
 
   config = {
-    bucket = var.backend_bucket
+    bucket = local.projects_backend_bucket
     prefix = "terraform/projects/${var.business_unit}/shared"
   }
 }

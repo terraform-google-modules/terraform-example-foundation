8c772dc61d934784468468d3eb4ebee521b68936
Jorge <iamjorge@google.com>
2022-10-24 11:27:13 -0500

    first

--- 0-bootstrap/main.tf
+++ 0-bootstrap/main.tf
@@ -1,10 +1,5 @@
 /**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ 
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -94,4 +89,3 @@ module "seed_bootstrap" {
 
   sa_org_iam_permissions = []
 }
-

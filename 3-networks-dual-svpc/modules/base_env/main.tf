15b967c00e78d78237c8fb8cd61c79ec967e7e24
Daniel da Silva Andrade <dandrade@ciandt.com>
2022-12-07 18:13:37 -0300

    fix: remove locals related to hub and spoke from dual shared vpc code

--- 3-networks-dual-svpc/modules/base_env/main.tf
+++ 3-networks-dual-svpc/modules/base_env/main.tf
@@ -32,16 +32,6 @@ locals {
 
 
   bgp_asn_number = var.enable_partner_interconnect ? "16550" : "64514"
-  /*
-   * Base network ranges
-   */
-  base_subnet_aggregates = ["10.0.0.0/16", "10.1.0.0/16", "100.64.0.0/16", "100.65.0.0/16"]
-  base_hub_subnet_ranges = ["10.0.0.0/24", "10.1.0.0/24"]
-  /*
-   * Restricted network ranges
-   */
-  restricted_subnet_aggregates = ["10.8.0.0/16", "10.9.0.0/16", "100.72.0.0/16", "100.73.0.0/16"]
-  restricted_hub_subnet_ranges = ["10.8.0.0/24", "10.9.0.0/24"]
 
   supported_restricted_service = [
     "accessapproval.googleapis.com",

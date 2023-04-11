a855335486f4bf14b605eb5a4abd549257e0cddc
Daniel Andrade <dandrade@ciandt.com>
2023-04-11 02:04:31 -0300

    chore: add helper steps execution functions for foundation deploy tasks (#948)

---
+++ helpers/deployer/steps/steps.go
@@ -0,0 +1,235 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package steps
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+const (
+	completedStatus = "COMPLETED"
+	destroyedStatus = "DESTROYED"
+	failedStatus    = "FAILED"
+	pendingStatus   = "PENDING"
+)
+
+type Step struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+	Error  string `json:"error"`
+}
+
+type Steps struct {
+	File  string          `json:"file"`
+	Steps map[string]Step `json:"steps"`
+}
+
+// String creates a string representation of the step
+func (s Step) String() string {
+	if s.Error == "" {
+		return fmt.Sprintf("%s %s", s.Name, s.Status)
+	}
+	return fmt.Sprintf("%s %s error:%s", s.Name, s.Status, s.Error)
+}
+
+// LoadSteps loads a previous execution steps from the given file.
+func LoadSteps(file string) (Steps, error) {
+	var s Steps
+	_, err := os.Stat(file)
+	if err != nil && !os.IsNotExist(err) {
+		return s, err
+	}
+	if os.IsNotExist(err) {
+		fmt.Printf("# creating new steps file '%s'.\n", file)
+		s = Steps{
+			File: file,
+		}
+	} else {
+		f, err := os.ReadFile(file)
+		if err != nil {
+			return s, err
+		}
+		err = json.Unmarshal(f, &s)
+		if err != nil {
+			return s, err
+		}
+		s.File = file
+	}
+	if s.Steps == nil {
+		s.Steps = map[string]Step{}
+	}
+	return s, nil
+}
+
+// SaveSteps saves the current execution state of the steps in the file that was loaded.
+func (s Steps) SaveSteps() error {
+	f, err := json.MarshalIndent(s, "", "    ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(s.File, f, 0644)
+}
+
+// CompleteStep marks a given step as completed.
+func (s Steps) CompleteStep(name string) error {
+	s.Steps[name] = Step{
+		Name:   name,
+		Status: completedStatus,
+	}
+	err := s.SaveSteps()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("# completing step '%s' execution\n", name)
+	return nil
+}
+
+// IsStepComplete checks if the given step is completed.
+func (s Steps) IsStepComplete(name string) bool {
+	v, ok := s.Steps[name]
+	if ok {
+		return v.Status == completedStatus
+	}
+	return false
+}
+
+// StepExists checks if the given step exists
+func (s Steps) StepExists(name string) bool {
+	_, ok := s.Steps[name]
+	return ok
+}
+
+// FailStep marks a given step as failed and saves the error message.
+func (s Steps) FailStep(name string, err string) error {
+	s.Steps[name] = Step{
+		Name:   name,
+		Status: failedStatus,
+		Error:  err,
+	}
+	e := s.SaveSteps()
+	if e != nil {
+		return e
+	}
+	fmt.Printf("# failing step '%s'. Failed with error: %s\n", name, err)
+	return nil
+}
+
+func isNested(name string) bool {
+	return strings.Contains(name, ".")
+}
+
+func parent(name string) string {
+	return strings.Split(name, ".")[0]
+}
+
+// ResetStep resets the execution status of a given step and its parent.
+func (s Steps) ResetStep(name string) error {
+	s.Steps[name] = Step{
+		Name:   name,
+		Status: pendingStatus,
+	}
+	err := s.SaveSteps()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("# resetting step '%s' execution\n", name)
+	if isNested(name) {
+		return s.ResetStep(parent(name))
+	}
+	return nil
+}
+
+// GetStepError gets the error message save in an step.
+func (s Steps) GetStepError(name string) string {
+	v, ok := s.Steps[name]
+	if ok {
+		return v.Error
+	}
+	return ""
+}
+
+// ListSteps lists the executed steps.
+func (s Steps) ListSteps() []string {
+	l := []string{}
+	for _, v := range s.Steps {
+		l = append(l, v.String())
+	}
+	sort.Strings(l)
+	return l
+}
+
+// RunStep executes a step and marks it as completed or failed.
+// Completed steps are not executed again.
+func (s Steps) RunStep(step string, f func() error) error {
+	if s.IsStepComplete(step) {
+		fmt.Printf("# skipping step '%s' execution\n", step)
+		return nil
+	}
+	fmt.Printf("# starting step '%s' execution\n", step)
+	err := f()
+	if err != nil {
+		e := s.FailStep(step, err.Error())
+		if e != nil {
+			return fmt.Errorf("error on FailStep %v, original error %w", e, err)
+		}
+		return err
+	}
+	return s.CompleteStep(step)
+}
+
+// IsStepDestroyed checks is the step was destroyed
+func (s Steps) IsStepDestroyed(name string) bool {
+	v, ok := s.Steps[name]
+	if ok {
+		return v.Status == destroyedStatus
+	}
+	return false
+}
+
+// DestroyStep destroys the given step
+func (s Steps) DestroyStep(name string) error {
+	s.Steps[name] = Step{
+		Name:   name,
+		Status: destroyedStatus,
+	}
+	err := s.SaveSteps()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("# destroying step '%s'\n", name)
+	return nil
+}
+
+// RunDestroyStep destroys a step and marks it as destroyed or failed.
+func (s Steps) RunDestroyStep(step string, f func() error) error {
+	if s.IsStepDestroyed(step) || !s.StepExists(step) {
+		fmt.Printf("# skipping step '%s' destruction\n", step)
+		return nil
+	}
+	fmt.Printf("# starting step '%s' destruction\n", step)
+	err := f()
+	if err != nil {
+		e := s.FailStep(step, err.Error())
+		if e != nil {
+			return fmt.Errorf("error on FailStep %v, original error %w", e, err)
+		}
+		return err
+	}
+	return s.DestroyStep(step)
+}
---
+++ helpers/deployer/steps/steps_test.go
@@ -0,0 +1,125 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package steps
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessSteps(t *testing.T) {
+	// Loading an existing file
+	e, err := LoadSteps("./testdata/existing.json")
+	assert.NoError(t, err)
+	assert.True(t, e.IsStepComplete("test"), "check if 'test' is 'COMPLETED' should be true")
+	assert.False(t, e.IsStepComplete("unit"), "check if 'unit' is 'COMPLETED' should be false")
+
+	// Loading a new file
+	s, err := LoadSteps(filepath.Join(t.TempDir(), "new.json"))
+
+	// CompleteStep
+	assert.False(t, s.IsStepComplete("unit"), "check if 'unit' is 'COMPLETED' should be false")
+	s.CompleteStep("unit")
+	assert.True(t, s.IsStepComplete("unit"), "check if 'unit' is 'COMPLETED' should be true")
+
+	// FailStep
+	msg := "step failed"
+	s.FailStep("fail", msg)
+	assert.False(t, s.IsStepComplete("fail"), "check if 'fail' is 'COMPLETED' should be false")
+	assert.Equal(t, s.GetStepError("fail"), msg, "step should have failed")
+
+	// DestroyStep
+	assert.False(t, s.IsStepDestroyed("old"), "check if 'old' is 'DESTROYED' should be false")
+	s.DestroyStep("old")
+	assert.True(t, s.IsStepDestroyed("old"), "check if 'old' is 'DESTROYED' should be true")
+
+	// ResetStep
+	s.CompleteStep("reset")
+	s.CompleteStep("reset.one")
+	assert.True(t, s.IsStepComplete("reset"), "check if 'reset is 'COMPLETED' should be true")
+	assert.True(t, s.IsStepComplete("reset.one"), "check if 'reset.one' is 'COMPLETED' should be true")
+
+	s.ResetStep("reset.one")
+
+	assert.False(t, s.IsStepComplete("reset.one"), "check if 'reset.one' is 'COMPLETED' should be false")
+	assert.False(t, s.IsStepComplete("reset"), "check if 'reset' is 'COMPLETED' should be false")
+
+	// RunStep
+	assert.False(t, s.IsStepComplete("good"), "check if 'good' is 'COMPLETED' should be false")
+	err = s.RunStep("good", func() error {
+		return nil
+	})
+	assert.NoError(t, err)
+	assert.True(t, s.IsStepComplete("good"), "check if 'good' is 'COMPLETED' should be true")
+
+	badStepMsg := "bad step"
+	assert.False(t, s.IsStepComplete("bad"), "check if 'bad' is 'COMPLETED' should be false")
+	err = s.RunStep("bad", func() error {
+		return fmt.Errorf(badStepMsg)
+	})
+	assert.Error(t, err)
+	assert.False(t, s.IsStepComplete("bad"), "check if 'bad' is 'COMPLETED' should be false")
+	assert.Equal(t, s.GetStepError("bad"), badStepMsg, "step 'bad' should have failed")
+
+	// complete states are not executed again
+	assert.True(t, s.IsStepComplete("good"), "check if 'good' is 'COMPLETED' should be true")
+	err = s.RunStep("good", func() error {
+		return fmt.Errorf("will fail if executed")
+	})
+	assert.NoError(t, err)
+	assert.True(t, s.IsStepComplete("good"), "check if 'good' is 'COMPLETED' should be true")
+
+	// RunDestroyStep
+	assert.False(t, s.IsStepDestroyed("unit"), "check if 'unit' is 'DESTROYED' should be false")
+	err = s.RunDestroyStep("unit", func() error {
+		return nil
+	})
+	assert.NoError(t, err)
+	assert.True(t, s.IsStepDestroyed("unit"), "check if 'unit' is 'DESTROYED' should be true")
+
+	s.CompleteStep("destroy")
+	assert.False(t, s.IsStepDestroyed("destroy"), "check if 'destroy' is 'DESTROYED' should be false")
+	err = s.RunDestroyStep("destroy", func() error {
+		return fmt.Errorf(badStepMsg)
+	})
+	assert.Error(t, err)
+	assert.False(t, s.IsStepDestroyed("destroy"), "check if 'destroy' is 'DESTROYED' should be false")
+	assert.Equal(t, s.GetStepError("destroy"), badStepMsg, "step 'destroy' should have failed")
+
+	s.DestroyStep("gone")
+	assert.True(t, s.IsStepDestroyed("gone"), "check if 'gone' is 'DESTROYED' should be true")
+	err = s.RunDestroyStep("gone", func() error {
+		return fmt.Errorf("will fail if executed")
+	})
+	assert.NoError(t, err)
+	assert.True(t, s.IsStepDestroyed("gone"), "check if 'gone' is 'DESTROYED' should be true")
+
+	// ListSteps
+	expectedSteps := []string{
+		"bad FAILED error:bad step",
+		"destroy FAILED error:bad step",
+		"fail FAILED error:step failed",
+		"gone DESTROYED",
+		"good COMPLETED",
+		"old DESTROYED",
+		"reset PENDING",
+		"reset.one PENDING",
+		"unit DESTROYED",
+	}
+	assert.ElementsMatch(t, expectedSteps, s.ListSteps())
+}

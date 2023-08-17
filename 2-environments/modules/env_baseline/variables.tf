9467318441d5d51d4619c5af4febc8241bfdb12d
Samir-Cit <samir@ciandt.com>
2023-08-17 16:52:01 -0300

    Fix lint.

--- 2-environments/modules/env_baseline/variables.tf
+++ 2-environments/modules/env_baseline/variables.tf
@@ -42,12 +42,12 @@ variable "project_budget" {
   alert_pubsub_topic: The name of the Cloud Pub/Sub topic where budget related messages will be published, in the form of `projects/{project_id}/topics/{topic_id}`.
   EOT
   type = object({
-    monitoring_budget_amount                = optional(number, 1000)
-    monitoring_alert_spent_percents         = optional(list(number), [0.5, 0.75, 0.9, 0.95])
-    monitoring_alert_pubsub_topic           = optional(string, null)
-    secret_budget_amount                    = optional(number, 1000)
-    secret_alert_spent_percents             = optional(list(number), [0.5, 0.75, 0.9, 0.95])
-    secret_alert_pubsub_topic               = optional(string, null)
+    monitoring_budget_amount        = optional(number, 1000)
+    monitoring_alert_spent_percents = optional(list(number), [0.5, 0.75, 0.9, 0.95])
+    monitoring_alert_pubsub_topic   = optional(string, null)
+    secret_budget_amount            = optional(number, 1000)
+    secret_alert_spent_percents     = optional(list(number), [0.5, 0.75, 0.9, 0.95])
+    secret_alert_pubsub_topic       = optional(string, null)
   })
   default = {}
 }

144b9eb49550fc389d764ca51af6b3a3789f7653
Andrew Peabody <andrewpeabody@google.com>
2023-11-10 15:52:40 -0800

    Update versions.tf

--- 5-app-infra/business_unit_1/development/versions.tf
+++ 5-app-infra/business_unit_1/development/versions.tf
@@ -21,12 +21,12 @@ terraform {
 
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.77, < 5.5"
+      version = ">= 3.77, < 6"
     }
 
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.77, < 5.5"
+      version = ">= 3.77, < 6"
     }
 
     null = {

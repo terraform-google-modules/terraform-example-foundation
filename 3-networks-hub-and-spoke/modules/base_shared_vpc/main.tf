ee3a1d819b7a81f8fb8195a57f0bbc4ce23c2809
Daniel Andrade <dandrade@ciandt.com>
2023-12-13 02:46:06 -0300

    feat: add support for fine grained configuration of VPC-flow logs (#1035)
    Co-authored-by: Grant Sorbo <gtsorbo@google.com>

--- 3-networks-hub-and-spoke/modules/base_shared_vpc/main.tf
+++ 3-networks-hub-and-spoke/modules/base_shared_vpc/main.tf
@@ -27,7 +27,7 @@ locals {
 
 module "main" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 7.0"
+  version = "~> 8.0"
 
   project_id                             = var.project_id
   network_name                           = local.network_name

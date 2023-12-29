8a98bee0de483ccab374789c22ae32b84575bd96
Leonardo Henrique Romanini <leonardo.romanini@ciandt.com>
2023-12-29 16:05:38 -0300

    Handle cases when the output is too large for github actions

--- 0-bootstrap/versions.tf
+++ 0-bootstrap/versions.tf
@@ -22,19 +22,18 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 3.50, != 4.31.0"
     }
+    // Un-comment github required_providers when using GitHub Actions
+    # github = {
+    #   source  = "integrations/github"
+    #   version = "5.34.0"
+    # }
+
+    // Un-comment tfe required_providers when using Terraform Cloud
+    # tfe = {
+    #   source  = "hashicorp/tfe"
+    #   version = "0.48.0"
+    # }
   }
-
-  // Un-comment github required_providers when using GitHub Actions
-  # github = {
-  #   source  = "integrations/github"
-  #   version = "5.34.0"
-  # }
-
-  // Un-comment tfe required_providers when using Terraform Cloud
-  # tfe = {
-  #   source  = "hashicorp/tfe"
-  #   version = "0.48.0"
-  # }
 
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-example-foundation:bootstrap/v3.0.0"

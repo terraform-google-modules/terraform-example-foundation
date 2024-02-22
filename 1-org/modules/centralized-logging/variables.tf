5c34cd551ada0037f8d01628c6acf532e875fe35
Renato Rudnicki <renatojr@ciandt.com>
2024-02-22 11:16:50 -0300

    Revert "create custom log bucket and sink for project destination"
    This reverts commit 57ec9f64d724476189443291694c8b45b844e8bb.

--- 1-org/modules/centralized-logging/variables.tf
+++ 1-org/modules/centralized-logging/variables.tf
@@ -30,19 +30,13 @@ variable "resource_type" {
 
   validation {
     condition     = contains(["project", "folder", "organization"], var.resource_type)
-    error_message = "The resource_type value must be: project, organization, billing_account or folder."
+    error_message = "The resource_type value must be: project, organization or folder."
   }
-}
-
-variable "logging_project_key" {
-  description = "(Optional) The key of logging destination project if it is inside resources map. It is mandatory when resource_type = project and logging_target_type = logbucket."
-  type        = string
 }
 
 variable "billing_account" {
   description = "Billing Account ID used in case sinks are under billing account level. Format 000000-000000-000000."
   type        = string
-  default     = "010ECE-40301B-50DDD5"
 }
 
 variable "enable_billing_account_sink" {
@@ -51,36 +45,15 @@ variable "enable_billing_account_sink" {
   default     = true
 }
 
+variable "logging_project_key" {
+  description = "(Optional) The key of logging destination project if it is inside resources map. It is mandatory when resource_type = project and logging_target_type = logbucket."
+  type        = string
+  default     = ""
+}
+
 variable "logging_destination_project_id" {
   description = "The ID of the project that will have the resources where the logs will be created."
   type        = string
-}
-
-variable "project_options" {
-  description = <<EOT
-Destination Project options:
-- logging_sink_name: The name of the log sink to be created.
-- logging_sink_filter: The filter to apply when exporting logs. Only log entries that match the filter are exported. Default is "" which exports all logs.
-- log_bucket_id: Id of the log bucket create to store the logs exported to the project.
-- log_bucket_description: Description of the log bucket create to store the logs exported to the project.
-- location: The location of the log bucket. Default: global.
-- enable_analytics: Whether or not Log Analytics is enabled in the _Default log bucket. A Log bucket with Log Analytics enabled can be queried in the Log Analytics page using SQL queries. Cannot be disabled once enabled.
-- retention_days: The number of days data should be retained for the _Default log bucket. Default 30.
-- linked_dataset_id: The ID of the linked BigQuery dataset for the _Default log bucket. A valid link dataset ID must only have alphanumeric characters and underscores within it and have up to 100 characters.
-- linked_dataset_description: A use-friendly description of the linked BigQuery dataset for the _Default log bucket. The maximum length of the description is 8000 characters.
-EOT
-  type = object({
-    logging_sink_name          = optional(string, null)
-    logging_sink_filter        = optional(string, "")
-    log_bucket_id              = optional(string, null)
-    log_bucket_description     = optional(string, null)
-    location                   = optional(string, "global")
-    enable_analytics           = optional(bool, true)
-    retention_days             = optional(number, 30)
-    linked_dataset_id          = optional(string, null)
-    linked_dataset_description = optional(string, null)
-  })
-  default = null
 }
 
 #----------------------------- #
@@ -161,3 +134,4 @@ EOT
   })
   default = null
 }
+

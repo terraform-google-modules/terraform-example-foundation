fc6693f8f53852148277c620eafeb3cd77aaddb1
Elliot <ellioteaton@gmail.com>
2024-05-31 15:11:54 +0000

    fix variable to manage explicit_dry_run_spec

--- 3-networks-hub-and-spoke/modules/restricted_shared_vpc/service_control.tf
+++ 3-networks-hub-and-spoke/modules/restricted_shared_vpc/service_control.tf
@@ -109,7 +109,7 @@ resource "google_access_context_manager_service_perimeter" "bridge_to_network_hu
   name           = "accessPolicies/${var.access_context_manager_policy_id}/servicePerimeters/${local.bridge_name}"
   title          = local.bridge_name
 
-  use_explicit_dry_run_spec = var.enforce_vpcsc
+  use_explicit_dry_run_spec = var.enforce_vpcsc ? false : true
 
   status {
     resources = formatlist("projects/%s", [var.project_number, var.restricted_net_hub_project_number])

d94dc1b3cf6aad80addf473616a8d597264b70bb
natthachai <natthachai@geniusall.net>
2024-07-05 09:37:54 +0000

    Initial commit for Nix environment configuration

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,8 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }:
+{
+  pkgs, ...
+}:
 {
   # Which nixpkgs channel to use.
   # channel = "stable-23.11"; # or "unstable"
@@ -26,15 +28,5 @@
       "vscodevim.vim"
     ];
   };
-
-  # Enable previews
-  previews = {
-    enable = true;
-    previews = {
-      web = {
-        command = "npm run dev";
-        manager = "web";
-      };
-    };
-  };
+}
 }

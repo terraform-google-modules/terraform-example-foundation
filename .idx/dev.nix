db8b02c0368d8efcd5da85ec564e87ea56019e20
natthachai <natthachai@geniusall.net>
2024-07-05 09:30:38 +0000

    Initial commit for Nix environment configuration

---
+++ .idx/dev.nix
@@ -0,0 +1,40 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }:
+{
+  # Which nixpkgs channel to use.
+  # channel = "stable-23.11"; # or "unstable"
+
+  # Add Packages
+  packages = [
+    pkgs.go
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.nodejs_20
+    pkgs.nodePackages.nodemon
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    MY_VAR = "my_value";
+  };
+
+  # IDX Settings
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "vscodevim.vim"
+    ];
+  };
+
+  # Enable previews
+  previews = {
+    enable = true;
+    previews = {
+      web = {
+        command = "npm run dev";
+        manager = "web";
+      };
+    };
+  };
+}

ecdd8f23cff5494a5ac2a45e291465a2526c9f0e
Daniel Andrade <dandrade@ciandt.com>
2024-11-01 15:02:45 -0300

    upgrade simple_bucket module

--- 4-projects/modules/base_env/example_storage_cmek.tf
+++ 4-projects/modules/base_env/example_storage_cmek.tf
@@ -44,7 +44,7 @@ resource "random_string" "bucket_name" {
 
 module "gcs_buckets" {
   source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
-  version = "~> 6.0"
+  version = "~> 8.0"
 
   project_id              = module.base_shared_vpc_project.project_id
   location                = var.location_gcs

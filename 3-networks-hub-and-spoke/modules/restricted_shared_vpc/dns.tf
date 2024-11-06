7f6644d35eaf41c1f07979ea221cc2701b5adc4c
Renato Rudnicki <renatojr@ciandt.com>
2024-11-06 11:38:49 -0300

    fix lint and module names

--- 3-networks-hub-and-spoke/modules/restricted_shared_vpc/dns.tf
+++ 3-networks-hub-and-spoke/modules/restricted_shared_vpc/dns.tf
@@ -57,11 +57,11 @@ module "peering_zone" {
 /******************************************
  DNS Forwarding
 *****************************************/
-module "dns-forwarding-zone" {
+module "dns_forwarding_zone" {
   source  = "terraform-google-modules/cloud-dns/google"
   version = "~> 5.0"
 
-  count   = var.mode != "spoke" ? 1 : 0
+  count = var.mode != "spoke" ? 1 : 0
 
   project_id = var.project_id
   type       = "forwarding"
@@ -69,7 +69,7 @@ module "dns-forwarding-zone" {
   domain     = var.domain
 
   private_visibility_config_networks = [
-    module.dns_hub_vpc.network_self_link
+    module.main.network_self_link
   ]
-  target_name_server_addresses = data.google_compute_network.vpc_dns_hub.self_link
+  target_network = data.google_compute_network.vpc_dns_hub.self_link
 }

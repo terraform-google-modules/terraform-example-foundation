aca34008b41e4dc16977c22130b785f015f4c9ca
Daniel Andrade <dandrade@ciandt.com>
2024-11-06 17:11:19 -0300

    fix: fix location variables for step 4-projects in deploy helper (#1334)

--- helpers/foundation-deployer/stages/apply.go
+++ helpers/foundation-deployer/stages/apply.go
@@ -338,8 +338,8 @@ func DeployProjectsStage(t testing.TB, s steps.Steps, tfvars GlobalTFVars, outpu
 	}
 	//for each environment
 	envTfvars := ProjEnvTfvars{
-		ProjectsKMSLocation: tfvars.ProjectsKMSLocation,
-		ProjectsGCSLocation: tfvars.ProjectsGCSLocation,
+		LocationKMS: tfvars.LocationKMS,
+		LocationGCS: tfvars.LocationGCS,
 	}
 	for _, envfile := range []string{
 		"development.auto.tfvars",

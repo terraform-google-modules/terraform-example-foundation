340c7d0079ecbc0aef23a64bc4b427f3a7426938
Renato Rudnicki <renatojr@ciandt.com>
2025-09-11 20:26:37 -0300

    remove cloudbuild_bucket_admin resource -test

--- 4-projects/business_unit_1/shared/example_infra_pipeline.tf
+++ 4-projects/business_unit_1/shared/example_infra_pipeline.tf
@@ -71,11 +71,11 @@ resource "google_storage_bucket_iam_member" "cloudbuild_sa_storage_admin" {
   member = "serviceAccount:tf-cb-builder-sa@${local.cloudbuild_project_id}.iam.gserviceaccount.com"
 }
 
-resource "google_storage_bucket_iam_member" "cloudbuild_bucket_admin" {
-  bucket = "${local.cloudbuild_project_id}_cloudbuild"
-  role   = "roles/storage.admin"
-  member = "serviceAccount:${module.app_infra_cloudbuild_project[0].sa}"
-}
+#resource "google_storage_bucket_iam_member" "cloudbuild_bucket_admin" {
+#  bucket = "${local.cloudbuild_project_id}_cloudbuild"
+#  role   = "roles/storage.admin"
+#  member = "serviceAccount:${module.app_infra_cloudbuild_project[0].sa}"
+#}
 
 module "app_infra_cloudbuild_project" {
   source = "../../modules/single_project"
@@ -133,7 +133,7 @@ resource "time_sleep" "wait_iam_propagation" {
     google_artifact_registry_repository_iam_member.builder_on_artifact_registry,
     google_project_iam_member.cloudbuild_logging,
     google_storage_bucket_iam_member.cloudbuild_sa_storage_admin,
-    google_storage_bucket_iam_member.cloudbuild_bucket_admin,
+    #google_storage_bucket_iam_member.cloudbuild_bucket_admin,
   ]
 }
 

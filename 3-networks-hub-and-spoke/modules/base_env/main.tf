d813ff872d3300a04ebc5aca130c69c3ae962d72
Daniel Andrade <dandrade@ciandt.com>
2025-10-06 11:08:01 -0300

    fix error Service 'alpha-documentai.googleapis.com' is not supported by VPC Service Controls

--- 3-networks-hub-and-spoke/modules/base_env/main.tf
+++ 3-networks-hub-and-spoke/modules/base_env/main.tf
@@ -26,7 +26,6 @@ locals {
     "adsdatahub.googleapis.com",
     "aiplatform.googleapis.com",
     "alloydb.googleapis.com",
-    "alpha-documentai.googleapis.com",
     "analyticshub.googleapis.com",
     "apigee.googleapis.com",
     "apigeeconnect.googleapis.com",

cc811c96b5c590fa63cb9aff89049bffca8ccce2
Daniel Andrade <dandrade@ciandt.com>
2025-12-01 16:50:03 -0300

    bump versions

--- 0-bootstrap/versions_cb.tf
+++ 0-bootstrap/versions_cb.tf
@@ -15,20 +15,20 @@
  */
 
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.3"
   required_providers {
     google = {
       // version 4.31.0 removed because of issue https://github.com/hashicorp/terraform-provider-google/issues/12226
       // version 6.26.0 and 6.27.0 removed because of the bug https://github.com/hashicorp/terraform-provider-google/issues/21950
       source  = "hashicorp/google"
-      version = ">= 3.50, != 4.31.0, != 6.26.0, != 6.27.0, < 7.0"
+      version = ">= 3.50, != 4.31.0, != 6.26.0, != 6.27.0, < 8.0"
     }
 
     google-beta = {
       // version 4.31.0 removed because of issue https://github.com/hashicorp/terraform-provider-google/issues/12226
       // version 6.26.0 and 6.27.0 removed because of the bug https://github.com/hashicorp/terraform-provider-google/issues/21950
       source  = "hashicorp/google-beta"
-      version = ">= 3.50, != 4.31.0, != 6.26.0, != 6.27.0, < 7.0"
+      version = ">= 3.50, != 4.31.0, != 6.26.0, != 6.27.0, < 8.0"
     }
   }
 

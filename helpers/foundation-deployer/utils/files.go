4266f51b86f7b3f8971edb2d947ddfa4febb53c2
cristianovi <cristianov@ciandt.com>
2026-05-18 10:06:41 -0300

    removing not used paramns on helper

--- helpers/foundation-deployer/utils/files.go
+++ helpers/foundation-deployer/utils/files.go
@@ -33,7 +33,7 @@ const (
 )
 
 // Allowed build types.
-var AllowedBuildTypes = []string{"cb", "github", "gitlab", "jenkins", "terraform_cloud", "local"}
+var AllowedBuildTypes = []string{"cb", "github", "gitlab", "jenkins", "terraform_cloud"}
 
 // CopyFile copies a single file from the src path to the dest path
 func CopyFile(src string, dest string) error {

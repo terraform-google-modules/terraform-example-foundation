799fe1e283ee95d72b831e0742e52cd46d74777b
marcos-leal-cit <marcosvl@ciandt.com>
2026-05-27 14:17:08 -0300

    fix: bump modules and submodules to google provider v7 (#1465)

--- 0-bootstrap/modules/parent-iam-remove-role/versions.tf
+++ 0-bootstrap/modules/parent-iam-remove-role/versions.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.77"
+      version = ">= 3.77, < 8.0"
     }
   }
 }
--- 5-app-infra/modules/env_base/versions.tf
+++ 5-app-infra/modules/env_base/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.61"
+      version = ">= 3.61, < 8.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.61"
+      version = ">= 3.61, < 8.0"
     }
     null = {
       source  = "hashicorp/null"

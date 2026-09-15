2f645857634db51522abb6d9af77f246e8a6d7e0
Daniel Andrade <dandrade@ciandt.com>
2026-09-15 10:47:27 -0300

    fix code location

--- 3-networks-hub-and-spoke/modules/partner_interconnect/main.tf
+++ 3-networks-hub-and-spoke/modules/partner_interconnect/main.tf
@@ -57,3 +57,47 @@ resource "google_compute_interconnect_attachment" "interconnect_attachment2_regi
   edge_availability_domain = "AVAILABILITY_DOMAIN_2"
   type                     = "PARTNER"
 }
+
+
+# ---------------------------------------------------------
+# NCC Spoke for Partner Interconnects - Region 1
+# ---------------------------------------------------------
+resource "google_network_connectivity_spoke" "region1_partner_ic_spoke" {
+  name        = "pic-spoke-${var.vpc_name}-${var.region1}"
+  project     = var.attachment_project_id
+  location    = var.region1
+  hub         = var.ncc_hub_uri
+  group       = var.ncc_hub_group
+  description = "NCC Spoke containing Partner Interconnects for region 1"
+
+  linked_interconnect_attachments {
+    # Reference the IDs natively from the google_compute_interconnect_attachment resources
+    uris = [
+      google_compute_interconnect_attachment.interconnect_attachment1_region1.id,
+      google_compute_interconnect_attachment.interconnect_attachment2_region1.id
+    ]
+
+    site_to_site_data_transfer = var.site_to_site_data_transfer
+  }
+}
+
+# ---------------------------------------------------------
+# NCC Spoke for Partner Interconnects - Region 2
+# ---------------------------------------------------------
+resource "google_network_connectivity_spoke" "region2_partner_ic_spoke" {
+  name        = "pic-spoke-${var.vpc_name}-${var.region2}"
+  project     = var.attachment_project_id
+  location    = var.region2
+  hub         = var.ncc_hub_uri
+  group       = var.ncc_hub_group
+  description = "NCC Spoke containing Partner Interconnects for region 2"
+
+  linked_interconnect_attachments {
+    uris = [
+      google_compute_interconnect_attachment.interconnect_attachment1_region2.id,
+      google_compute_interconnect_attachment.interconnect_attachment2_region2.id
+    ]
+
+    site_to_site_data_transfer = var.site_to_site_data_transfer
+  }
+}
